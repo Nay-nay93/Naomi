d0d554eec0a3bc75282e47c4de9739680c6c2055
Nay-nay93 <greconaomi@gmail.com>
2025-01-11 18:16:44 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,4 @@ document.getElementById("app").innerHTML = `
 <h1>Naomi's space</h1>
 <h2> Hosted on <a href= "https://www.netlify.com/" > Netlify </a> </h2>
 
-      <h2> Hosted on <a href= "https://www.netlify.com/" > Netlify </a> </h2>
-
 `;
